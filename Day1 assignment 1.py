69e7d1af86350d2f5986b98d138a4f28d3095bd0
sharmila4250 <66352207+sharmila4250@users.noreply.github.com>
2020-07-20 20:28:51 +0530

    Add files via upload

---
+++ Day1 assignment 1.py
@@ -0,0 +1,17 @@
+
+print ("What is python?")
+print("Python is an interpreted, object-oriented, high-level programming language with dynamic semantics. Its high-level built in data structures, combined with dynamic typing and dynamic binding, make it very attractive for Rapid Application Development, as well as for use as a scripting or glue language to connect existing components together.") 
+
+print("Ways to install")
+print("- PYTHON can be easily downloaded from the official website python.org and can be installed.")
+print("- Download Anaconda software and install.")
+
+print("2. What is code environment ?")
+
+print("Code Environment is a place where people can write the code, compile and execute the code to view the result.")
+print("-A code environment is a standalone and self-contained environment to run PYTHON or R code. Each code environment has its own set of packages. Environments are independent: you can install different packages or different versions of packages in different environments without interaction between them.")
+
+print("Types")
+print("1. Text Editors")
+print("2.Full IDEs")
+print("3.Notebook Environments")
