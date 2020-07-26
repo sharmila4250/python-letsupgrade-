d289d8c926954628b1849b3964ab134606ebe781
sharmila4250 <66352207+sharmila4250@users.noreply.github.com>
2020-07-26 08:29:09 +0530

    Add files via upload

---
+++ Python day1assignment1.py
@@ -0,0 +1,79 @@
+print("Basic Syntax of Python")
+print("   ")
+print("Hello world")
+print("""("")....("")""")
+print('Hello World')
+print("Python's World")
+print("Hello \n Python")
+print("Hello \t Python")
+print("Pyhon\'s World") 
+name="shu";
+marks=99.08;
+age=18;
+print("The name of person is",name, "marks is",marks,"age is",age)
+print("     ")
+print("Python Variable And Assignment Statement")
+print("  ")
+x=20; 
+y=20;
+print("x is:",x)
+print("y is:",y)
+del x;
+#print(x) undefined
+print("y is:", y)
+print("   ")
+print(" Operators ")
+print("   ")
+print("Arithmetic Operators")
+a=10;
+b=6;
+print(a*b)
+print(a+b)
+print(a-b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
+print("  ")
+print("Relational Operator")
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a==b)
+print(a!=b)
+print(" ")
+print("Assignment Operator")
+a+=b
+print(a)
+a-=b
+print(a)
+a*=b
+print(a)
+a/=b
+print(a)
+a**=b
+print(a)
+print("  ")
+print("Bitwise Operator")
+print(a|b)
+print(a&b)
+print(~a)
+print(a^b)
+print(a<<b)
+print(a>>b)
+print("  ")
+print("Membership Operator")
+str="python program"
+bool="p" in str;
+print(bool)
+bool="y" not in str;
+print(bool)
+print("   ")
+print("Identity Operator")
+c=a is b
+print(c)
+c=a is not b
+print(c)
+id=id(a)
+print(id)
