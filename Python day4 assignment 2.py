80cd8e21df98a478b8326e01d390b0b560cb4ef4
sharmila4250 <66352207+sharmila4250@users.noreply.github.com>
2020-07-26 16:34:29 +0530

    Add files via upload

---
+++ Python day4 assignment 2.py
@@ -0,0 +1,28 @@
+#python 3.7.1
+
+print ("Hello, Dcoder!")
+str="hello"
+x=str.islower()
+print(x)
+str1="heo23"
+y=str1.islower()
+print(y)
+str4="he$%o23"
+m=str4.islower()
+print(m)
+str2="hOo23"
+z=str2.islower()
+print(z)
+str3="HELLO"
+p=str3.isupper()
+print(p)
+str5="HELL45O"
+a=str5.isupper()
+print(a)
+str6="HELLO$$"
+k=str6.isupper()
+print(k)
+
+str7="HELLOaaa"
+d=str7.isupper()
+print(d)
