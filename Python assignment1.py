74e59cd6905e06331bb5df385f6981982f408e2d
sharmila4250 <66352207+sharmila4250@users.noreply.github.com>
2020-07-26 09:36:44 +0530

    Add files via upload

---
+++ Python assignment1.py
@@ -0,0 +1,8 @@
+sum1=0
+num1=int(input("enter the starting number "))
+num2=int(input("enter the ending number "))
+while num1<num2:
+  sum1=sum1+num1;
+  print(sum1)
+  num1=num1+1
+print("sum is",sum1);
