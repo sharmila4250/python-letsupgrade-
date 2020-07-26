25570b64ebdd73f8d03af24d689dd84b86bc02c7
sharmila4250 <66352207+sharmila4250@users.noreply.github.com>
2020-07-26 16:33:41 +0530

    Add files via upload

---
+++ Python day4 assignment 1.py
@@ -0,0 +1,13 @@
+#python 3.7.1
+
+print ("Hello, Dcoder!")
+str1="what we think we become and we will complete our goals "
+u=str1.find("we")
+print(u)
+v=str1.rfind("we")
+print(v)
+'''t=str1[6:28].count("we")
+print("total no. of we between 6 and 28 is",t)'''
+y=str1[6:28].find("we")
+z=u+1+y
+print(z)
